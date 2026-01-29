f233d2672c8aeec73f2b3e0b5ea2d9c274a14e4c
shayphillips <shay.p0222@gmail.com>
2026-01-29 17:26:57 -0600

    Initial Commit

---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Shay Phillips on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
