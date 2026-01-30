36ab29049dda6deb9f699ca5689f7a32034feb04
shayphillips <shay.p0222@gmail.com>
2026-01-29 18:16:18 -0600

    Sliders started

--- TipCalculatorApp/ContentView.swift
+++ TipCalculatorApp/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // State variables
+    @State var billAmount: Double = 0.0
+    @State var tipPercentage: Double = 0.0
+    @State var numberOfPeople: Double = 0.0
+    @State var showResults: Bool = false
+
+    // Computed variables
+    var tipAmount: Double = 0.0
+    var totalAmount: Double = 0.0
+    var amountPerPerson: Double = 0.0
+    
     var body: some View {
+        // Title
+        Text("Tip Calculator")
+            .font(.title)
+            .fontWeight(.bold)
+
+            Spacer()
+        
+        // Slider Stack
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            // Bill Amount Slider
+            HStack{
+                Text("Bill Amount:")
+                    .frame(width: 120, height: 30)
+
+                Text("\(billAmount, format: .currency(code: "USD"))")
+                    .frame(width: 150, height: 30)
+            }
+            
+            Slider(value: $billAmount, in: 0...500, step: 0.01)
+                .frame(width: 370, height: 50)
+            
+            
+            // Tip Percentage Slider
+            HStack{
+                Text("Tip Percentage:")
+                    .frame(width: 120, height: 30)
+
+                Text("\(tipPercentage)")
+                    .frame(width: 80, height: 30)
+            }
+            
+            Slider(value: $tipPercentage, in: 0...30, step: 1.0)
+                .frame(width: 370, height: 50)
+
+            
+            // Number of People Slider
+
+
+            
+
         }
-        .padding()
+        .padding(.vertical, 300)
     }
 }
+
 
 #Preview {
     ContentView()
